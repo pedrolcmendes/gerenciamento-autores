3319c68eee241c4613ba5b30476fe134a20d64a6
pedrolcmendes <pepelmendes05@gmail.com>
2026-03-24 14:39:09 -0300

    primeiro commit

---
+++ autores/src/test/java/com/exercicio/autores/AutoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercicio.autores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
